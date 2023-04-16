e0abc41472940f84da512ffa438ca1e191e2c9be
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2023-04-16 13:17:31 +0500

    Update CreateStructure.sql
    Added a comment about the task

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -1,3 +1,12 @@
+-- Создать таблицы
+--     dbo.SKU (ID identity, Code, Name)
+--         * Ограничение на уникальность поля Code
+--         * Поле Code вычисляемое: "s" + ID
+--     dbo.Family (ID identity, SurName, BudgetValue)
+--     dbo.Basket (ID identity, ID_SKU (внешний ключ на таблицу dbo.SKU), ID_Family (внешний ключ на таблицу dbo.Family) Quantity, Value, PurchaseDate, DiscountValue)
+--         * Ограничение, что поле Quantity и Value не могут быть меньше 0
+--         * Добавить значение по умолчанию для поля PurchaseDate: дата добавления записи (текущая дата)
+
 CREATE TABLE SKU (
     ID INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
     Code AS ('s' + CONVERT(VARCHAR(255), ID)) UNIQUE,
